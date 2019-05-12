c72e4a603d65fbe938dcdfe19defa5f3cdbe673a
JamesDolphin <j.dolphin2013@gmail.com>
2019-05-12 16:02:15 +1000

    .

--- src/scrape_cleaner.js
+++ src/scrape_cleaner.js
@@ -2,6 +2,11 @@ const request = require('request');
 const cheerio = require('cheerio');
 
 const matchArray = [];
+const teamList = [];
+
+function onlyUnique(value, index, self) {
+  return self.indexOf(value) === index;
+}
 
 const getMatches = date => new Promise((resolve, reject) => {
   const url = `https://play.esea.net/index.php?s=league&d=schedule&date=${date}&game_id=25&division_level=premier`;
@@ -9,13 +14,24 @@ const getMatches = date => new Promise((resolve, reject) => {
     if (!error && response.statusCode === 200) {
       const $ = cheerio.load(html);
 
-      $('.match-overview table tbody').each((i, n) => {
-        matchArray.push(`${date} ${$('.match-overview table td').filter((i, n) => !$(n).hasClass('stat')).map((i, n) => $(n).text().trim()).toArray()
-          .join(' vs ')}`);
-      });
 
-      // put a break at line 35. Output is almost correct. Need each element to only contain 1 match not all on that date
+      const A = $('.match-overview table td').filter((i, n) => !$(n).hasClass('stat')).map((i, n) => $(n).text().trim()).toArray()
+        .join(' ');
 
+
+      teamList.push(A);
+
+
+      // $('.match-overview table tbody').each((i, el) => {
+      //   let newMatch = $(el).text();
+      //   newMatch = newMatch.replace(/ *\([^)]*\) */g, ' ');
+      //   newMatch = newMatch.replace(/,/g, '');
+      //   newMatch = newMatch.replace(/[()]/g, ' ');
+      //   newMatch = newMatch.replace(/H1H2OTT/g, '');
+      //   newMatch = newMatch.replace(/ {2}/g, ' ');
+
+      //   matchArray.push(`${date}${newMatch}`);
+      // });
 
       // resolve();
     } else {
@@ -26,12 +42,23 @@ const getMatches = date => new Promise((resolve, reject) => {
 });
 
 const doAllDates = async () => {
-  const now = new Date(2019, 4, 30);
+  const now = new Date();
   for (let d = new Date(2019, 3, 28); d <= now; d.setDate(d.getDate() + 1)) {
     const date = new Date(d);
     const dateFormatted = (`${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`);
-    await getMatches(dateFormatted); // how can i wait for EVERY iteration to finish here
+    await getMatches(dateFormatted);
   }
+
+  for (let x = 0; x < teamList.length; x += 1) {
+    teamList[x] = teamList[x].replace(/ *\([^)]*\) */g, '');
+    console.log(teamList[x]);
+
+    teamList[x].split(' ');
+  }
+
+
+  // teamList = teamList.filter(onlyUnique);
+  // console.log(teamList);
   console.log(matchArray);
 };
 
